f149b1aa7ad404054bac8c045601352d6beef117
iaoenang@gmail.com <jingjing*912490>
2016-02-07 18:45:30 +0800

    common response重命名

---
+++ devpkg-android/src/main/java/org/senydevpkg/net/resp/ErrorResponse.java
@@ -0,0 +1,32 @@
+package org.senydevpkg.net.resp;
+
+/**
+ * ━━━━ Code is far away from ━━━━━━
+ * 　　  () 　　　  ()
+ * 　　  ( ) 　　　( )
+ * 　　  ( ) 　　　( )
+ * 　　┏┛┻━━━┛┻┓
+ * 　　┃　　　━　　　┃
+ * 　　┃　┳┛　┗┳　┃
+ * 　　┃　　　┻　　　┃
+ * 　　┗━┓　　　┏━┛
+ * 　　　　┃　　　┃
+ * 　　　　┃　　　┗━━━┓
+ * 　　　　┃　　　　　　　┣┓
+ * 　　　　┃　　　　　　　┏┛
+ * 　　　　┗┓┓┏━┳┓┏┛
+ * 　　　　　┃┫┫　┃┫┫
+ * 　　　　　┗┻┛　┗┻┛
+ * ━━━━ bug with the XYY protecting━━━
+ * <p/>
+ * Created by Seny on 2016/1/17.
+ * 错误响应接口，定义服务器返回的错误信息
+ */
+public abstract class ErrorResponse implements IResponse {
+    /**
+     * 返回服务器返回的错误提醒信息。将通过Toast的形式提醒用户。
+     *
+     * @return
+     */
+    public abstract String getErrorMsg();
+}
---
+++ devpkg-android/src/main/java/org/senydevpkg/net/resp/IResponse.java
@@ -0,0 +1,28 @@
+package org.senydevpkg.net.resp;
+
+/**
+ * ━━━━ Code is far away from ━━━━━━
+ * 　　  () 　　　  ()
+ * 　　  ( ) 　　　( )
+ * 　　  ( ) 　　　( )
+ * 　　┏┛┻━━━┛┻┓
+ * 　　┃　　　━　　　┃
+ * 　　┃　┳┛　┗┳　┃
+ * 　　┃　　　┻　　　┃
+ * 　　┗━┓　　　┏━┛
+ * 　　　　┃　　　┃
+ * 　　　　┃　　　┗━━━┓
+ * 　　　　┃　　　　　　　┣┓
+ * 　　　　┃　　　　　　　┏┛
+ * 　　　　┗┓┓┏━┳┓┏┛
+ * 　　　　　┃┫┫　┃┫┫
+ * 　　　　　┗┻┛　┗┻┛
+ * ━━━━ bug with the XYY protecting━━━
+ * <p/>
+ *
+ * Created by Seny on 2015/12/1.
+ *
+ * 接口IResponse 用于封装服务器返回的数据，定义接口，用于HttpLoader的通用性。
+ */
+public interface IResponse {
+}
